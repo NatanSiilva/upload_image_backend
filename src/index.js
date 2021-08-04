6a783b011b71a053bd6b1b71b1d5efc0aa3c75af
Natanael Silva <natanaelsilva.progjs@gmail.com>
2021-08-03 23:59:31 -0300

    feat: last commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
+
+// Database setup
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
+);
 
 app.use(require("./routes"));
 
